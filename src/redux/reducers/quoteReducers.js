2c27d002d4216e9d7abe71d2bf3d40e38f88a310
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-09 14:44:32 -0400

    Use the initial state to handle the actions on quotes

--- src/redux/reducers/quoteReducers.js
+++ src/redux/reducers/quoteReducers.js
@@ -1,12 +1,12 @@
 const initialState = { text: '', author: '' };
 
 const quoteReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case 'SET_QUOTE':
-            return action.payload;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'SET_QUOTE':
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 export default quoteReducer;
