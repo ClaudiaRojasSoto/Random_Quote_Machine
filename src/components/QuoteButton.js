488cbf08ed92b4f890efd8b458a5bea7ed698d77
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-09 14:41:00 -0400

    Add the proptypes fot onclick

--- src/components/QuoteButton.js
+++ src/components/QuoteButton.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './QuoteButton.css'
+import PropTypes from 'prop-types';
+import './QuoteButton.css';
 
 function QuoteButton({ onClick }) {
   return (
-    <button id="new-quote" onClick={onClick}>Nueva Cita</button>
+    <button id="new-quote" type="button" onClick={onClick}>Nueva Cita</button>
   );
 }
+
+QuoteButton.propTypes = {
+  onClick: PropTypes.string.isRequired,
+};
 
 export default QuoteButton;
